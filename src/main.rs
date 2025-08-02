cb70868ff0191b7a45aed24be045ba69327f2fdc
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-08-02 21:41:39 +0100

    health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -6,6 +6,7 @@ async fn main() -> Result<(), std::io::Error> {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
@@ -15,4 +16,8 @@ async fn main() -> Result<(), std::io::Error> {
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
