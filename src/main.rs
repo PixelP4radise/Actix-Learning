8cbf777cc39a0d80ce020c8541bd453e72aaea03
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-08-03 21:30:35 +0100

    getting configs from a file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
-use actix_learning::startup::run;
+use actix_learning::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port or ip");
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to bind to port or ip");
     run(listener)?.await
 }
