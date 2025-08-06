1565247662803ac441849b2176474ecd2891845a
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-08-06 16:32:05 +0100

    cleanup being made

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
 use std::net::TcpListener;
 
-use actix_learning::{configuration::get_configuration, startup::run};
+use actix_learning::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    LogTracer::init().expect("Failed to set logger.");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("actix-learning".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("actix-learning".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
@@ -28,6 +19,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
-    let listener = TcpListener::bind(address).expect("Failed to bind to port or ip");
-    run(listener, connection_pool)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
